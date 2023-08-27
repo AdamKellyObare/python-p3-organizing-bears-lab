c9cde39b62813463309e1d589201046ff920143a
AdamKelly <adam.obare@student.moringaschool.com>
2023-08-27 09:46:28 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+CREATE TABLE (
+    (1, 'Mr. Chocolate', 12, 'M', 'fat', 'calm', 0),
+    (2, 'Rowdy', 12, 'M', 'tall', 'energetic', 0),
+    (3, 'Tabitha', 12, 'F', 'slim', 'friendly', 0),
+    (4, 'Sergeant Brown', 12, 'M', 'slender', 'assertive', 0),
+    (5, 'Melissa', 12, 'F', 'fat', 'gentle', 1),
+    (6, 'Grinch', 12, 'M', 'fat', 'mischievous', 1),
+    (7, 'Wendy', 12, 'F', 'musculine', 'aggressive', 1),
+    (7, NULL, 12, 'F', 'slender', 'aggressive', 1);
+);
